8f63e59aedf9bb6acf4e16317129dc99b697993c
Starship Engineer <StarshipEngineer@users.noreply.github.com>
2020-06-30 11:35:13 -0500

    Adds edits to server.conf

--- config-server.sh
+++ config-server.sh
@@ -4,12 +4,14 @@
 cp /usr/share/doc/openvpn/examples/sample-config-files/server.conf.gz /etc/openvpn/
 gzip -d /etc/openvpn/server.conf.gz
 
-#edit HMAC
+#Enable HMAC
 sed -i '/;tls-auth ta.key 0 # This file is secret/ctls-auth ta.key 0 # This file is secret' /etc/server.conf
 
+#Edit the cryptographic cipher
+sed -i '/;cipher AES-256-CBC/ccipher AES-256-CBC' /etc/server.conf
 
-#edit cipher
-#add HMAC algorithm
+#Add HMAC algorithm
+sed -i '/cipher AES-256-CBC/aauth SHA256' /etc/server.conf
 
 #Align Diffie-Hellman filename
 sed -i '/dh dh2048.pem/cdh dh.pem' /etc/server.conf
